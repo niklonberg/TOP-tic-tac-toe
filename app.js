5a2e9c6b744f71fc5f36ff8ed3c7a3472891e3bf
Nikolai Lønberg <niklonberg@gmail.com>
2023-10-30 14:20:45 +0100

    create Gameboard IIFE factory, with starting array of markers

---
+++ app.js
@@ -0,0 +1,5 @@
+const log = console.log;
+import { Gameboard } from "./scripts/modules/Gameboard.js";
+
+log(Gameboard)
+
---
+++ scripts/modules/Gameboard.js
@@ -0,0 +1,4 @@
+export const Gameboard = (function () {
+  const gameField = ["x", "o", , "x", , "o", "x", "o"];
+  return {gameField}
+});
