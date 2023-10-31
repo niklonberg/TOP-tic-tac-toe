be89b0e8b95d52cab9c6c6045bb9075e8b2ffbf1
Nikolai Lønberg <niklonberg@gmail.com>
2023-10-31 10:10:29 +0100

    add player limit count, ensuring no more than two players can exist in the game at a time

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ import { GameController } from "./scripts/modules/GameController.js";
 
 const john = createPlayer("john");
 const jane = createPlayer("jane");
+const jim = createPlayer("jim");
 
 log(GameController);
 log(john);
-log(GameController.gameState.getField());
+log(GameController.board.getField());
 log(GameController.addPlayer(john));
+log(GameController.addPlayer(jane));
+log(GameController.addPlayer(jim));
 log(GameController);
--- scripts/modules/GameController.js
+++ scripts/modules/GameController.js
@@ -1,12 +1,16 @@
 import { Gameboard } from "./Gameboard.js";
 
 export const GameController = (function () {
-  const gameState = Gameboard;
+  const board = Gameboard;
   const players = {};
 
   const addPlayer = (player) => {
-    players[player.name] = player;
+    if (Object.keys(players).length < 2) {
+      players[player.name] = player;
+    } else {
+      throw new Error("Player count limit reached");
+    }
   };
 
-  return { gameState, players, addPlayer };
+  return { board, players, addPlayer };
 })();
