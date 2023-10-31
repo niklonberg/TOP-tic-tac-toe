80f8de9f45ef28753824de669de9e5eb3a260c7e
Nikolai Lønberg <niklonberg@gmail.com>
2023-10-31 10:52:46 +0100

    add private marker variable to createPlayer factory, add getMarker method

--- scripts/modules/CreatePlayer.js
+++ scripts/modules/CreatePlayer.js
@@ -1,9 +1,12 @@
 export const createPlayer = function (name, marker) {
   let score = 0;
+  const mark = marker;
 
   const getScore = () => score;
 
   const incrementScore = () => ++score;
 
-  return { name, marker, getScore, incrementScore };
+  const getMarker = () => mark;
+
+  return { name, getScore, incrementScore, getMarker };
 };
--- scripts/modules/Gameboard.js
+++ scripts/modules/Gameboard.js
@@ -1,9 +1,10 @@
 export const Gameboard = (function () {
   const gameField = new Array(9).fill(null);
+
   const getField = () => gameField;
 
   const addMarker = (index, player) => {
-    gameField[index] = player.marker;
+    gameField[index] = player.getMarker();
   };
 
   return { getField, addMarker };
