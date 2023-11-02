973e9e985b17640c7ac8f6de0224ccafbab6e59e
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-02 10:07:38 +0100

    add createFieldItem, renderBoard methods to DisplayController

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const log = console.log;
 
 import createPlayer from "./scripts/modules/CreatePlayer.js";
 import GameController from "./scripts/modules/GameController.js";
+import DisplayController from "./scripts/modules/DisplayController.js";
 
 const john = createPlayer("john", "x", true);
 const jane = createPlayer("jane", "o");
+
+DisplayController.renderBoard();
 
 GameController.addPlayer(john);
 GameController.addPlayer(jane);
--- scripts/modules/DisplayController.js
+++ scripts/modules/DisplayController.js
@@ -2,8 +2,27 @@ import Gameboard from "./Gameboard.js";
 
 const DisplayController = (function () {
   const gameField = Gameboard.getField();
+  const gameContainer = document.querySelector("#game-board");
+  const activePlayerMsgBox = document.querySelector("#active-player");
 
-  const renderBoard = () => {};
+  const createFieldItem = () => {
+    const btn = document.createElement("button");
+    /* add class, id etc. */
+    btn.textContent = "click"; /* placeholder text */
+    return btn;
+  };
+
+  const renderBoard = () => {
+    gameField.forEach((_, index) => {
+      const btn = createFieldItem();
+      btn.dataset.index = index;
+      gameContainer.appendChild(btn);
+    });
+  };
+
+  const updateBoard = () => {};
+
+  return { createFieldItem, renderBoard, updateBoard };
 })();
 
 export default DisplayController;
