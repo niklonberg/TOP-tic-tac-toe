99edeed749044e85dc6536d7b6994ae203f4b078
Nikolai Lønberg <niklonberg@gmail.com>
2024-04-19 11:27:45 +0200

    remove unused code

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-const log = console.log;
-
-/* import GameController from "./scripts/modules/GameController.js";
-import GameDisplayController from "./scripts/modules/GameDisplayController.js"; */
 import UserController from "./scripts/modules/UserController.js";
 
 UserController.getStoredUsers();
